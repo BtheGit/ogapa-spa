50ba34509307271da73cde180538b2d1fcf19e2a
BtheGit <BrendanWebDev@gmail.com>
2018-07-24 17:00:39 -0400

    Add thunk support

--- src/services/middleware.js
+++ src/services/middleware.js
@@ -1,8 +1,9 @@
 import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
 import history from './history';
 
-const middleware = composeWithDevTools(applyMiddleware(routerMiddleware(history)));
+const middleware = composeWithDevTools(applyMiddleware(thunk, routerMiddleware(history)));
 
 export default middleware;
