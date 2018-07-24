170a5452262490afba75d61b1664f13bfebb177d
BtheGit <BrendanWebDev@gmail.com>
2018-07-24 15:33:43 -0400

    Add Home View

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -5,11 +5,13 @@ import {
     Route
 } from 'react-router-dom';
 import SearchPage from './views/search_page/SearchPage';
+import HomePage from './views/home_page/HomePage';
 
 const AppRouter = ({ history }) => (
   <Router history={ history }>
     <Switch>
-        <Route exact path="*" component={ SearchPage } />
+        <Route exact path="/search:id" component={ SearchPage } />
+        <Route path="*" component={ HomePage } /> 
     </Switch>
   </Router>
 )
---
+++ src/views/home_page/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Search from '../../components/search/Search';
+
+export class HomePage extends React.Component {
+    static propTypes = {
+
+    }
+
+    static defaultProps = {
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    render(){
+        return(
+            <div>
+                <Search />
+            </div>
+        )
+    }
+};
+
+export default HomePage;
