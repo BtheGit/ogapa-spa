9b24d6c0bbb44be4059732fc2d1efada40c7409f
BtheGit <BrendanWebDev@gmail.com>
2018-08-04 20:23:45 -0400

    Add base RecordPage styles

--- src/views/record_page/RecordPage.js
+++ src/views/record_page/RecordPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { retrieveRecord } from '../../services/api/actions';
 import Spinner from '../../components/spinner/Spinner';
+import './RecordPage.css';
 export class RecordPage extends React.Component {
     static propTypes = {
 
@@ -24,21 +25,32 @@ export class RecordPage extends React.Component {
                     (this.props.record && this.props.record["award_id_piid"]) && !this.props.isFetching
                         ?
                         <React.Fragment>
-                            <h1>Contract Summary</h1>
-                            <h2>Awarding Agency</h2>
-                            <p>Awarding Agency: { this.props.record['awarding_agency_name'] }</p>
-                            <p>Awarding Sub Agency: { this.props.record['awarding_sub_agency_name'] }</p>
-                            <h2>Funding Agency</h2>
-                            <p>Funding Agency: { this.props.record['funding_agency_name'] }</p>
-                            <p>Funding Sub Agency: { this.props.record['funding_sub_agency_name'] }</p>
-                            <h2>Recipient</h2>
-                            <p>Recipient: { this.props.record['recipient_name'] }</p>
-                            <p>Duns: { this.props.record['recipient_duns'] }</p>
-                            <p>Parent Duns: { this.props.record['recipient_parent_duns'] }</p>
-                            <h2>Award</h2>
-                            <p>Award Description: { this.props.record['award_description'] }</p>
-                            <p>Current Award Amount: { this.props.record['current_total_value_of_award'] }</p>
-                            <p>Potential Award Amount: { this.props.record['potential_total_value_of_award'] }</p>
+                            <header className="record__header">
+                                <h1>Contract Summary</h1>
+                                <p>{ this.props.record['award_id_piid']}</p>
+                            </header>
+                            <main className="record__main">
+                                <div className="record__award">
+                                    <h2>Award <span>({ this.props.record['award_id_piid']})</span></h2>
+                                    <p>Award Description: { this.props.record['award_description'] }</p>
+                                    <p>Current Award Amount: ${ this.props.record['current_total_value_of_award'] }</p>
+                                    <p>Potential Award Amount: ${ this.props.record['potential_total_value_of_award'] }</p>
+                                </div>
+                                <div className="record__agency">
+                                    <h2>Awarding Agency</h2>
+                                    <p>{ this.props.record['awarding_agency_name'] }</p>
+                                    <p>Awarding Sub Agency: { this.props.record['awarding_sub_agency_name'] }</p>
+                                    <h2>Funding Agency</h2>
+                                    <p>{ this.props.record['funding_agency_name'] }</p>
+                                    <p>Funding Sub Agency: { this.props.record['funding_sub_agency_name'] }</p>
+                                </div>
+                                <div className="record__recipient">
+                                    <h2>Recipient</h2>
+                                    <p>{ this.props.record['recipient_name'] }</p>
+                                    <p>Duns: { this.props.record['recipient_duns'] }</p>
+                                    <p>Parent Duns: { this.props.record['recipient_parent_duns'] }</p>
+                                </div>
+                            </main>
                         </React.Fragment>
                         :
                             null
